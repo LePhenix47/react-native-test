949971fc5ef27139ab8c585fec09a70e416e837d
Younes Lahouiti <loller763@gmail.com>
2023-06-29 09:06:16 +0200

    Change of file extension: JS to TS

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from 'expo-router';
 import React from 'react'
 import { View,Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home():JSX.Element {
+    const route = useRouter() ;
   return (
-    <View>
-        <Text>Hello world! Testing</Text>
-    </View>
+    <SafeAreaView>
+        <Text>Hello world! Now a safe area view</Text>
+    </SafeAreaView>
   )
 }
